e71aaa6c165465896fbbca52b4b6567b2d94c379
david <davidgrong778@gmail.com>
2022-09-22 20:43:41 -0500

    async

--- numberApp.js
+++ numberApp.js
@@ -2,20 +2,33 @@ BASE_URL = 'http://numbersapi.com'
 
 
 
-const favNumber = axios.get(`${BASE_URL}/7?json`);
-favNumber
-    .then(data => {
-        console.log(data['data'].text);
-    });
+async function asyncFavNumber(num){
+    const favNumber = await axios.get(`${BASE_URL}/${num}?json`);
+    console.log(favNumber.data.data.text);
+}
+asyncFavNumber(7);
 
-let numbers = [8, 5, 10];
-const multiNumbers = axios.get(`${BASE_URL}/${numbers}?json`);
-multiNumbers
-    .then(data => {
-        for(const num of numbers){
-            console.log(data['data'][`${num}`]);
-        }
-    })
+// const favNumber = axios.get(`${BASE_URL}/7?json`);
+// favNumber
+//     .then(data => {
+//         console.log(data['data'].text);
+//     });
+
+async function asyncMultiNumbers(numbers){
+    const multiNumbers = await axios.get(`${BASE_URL}/${numbers}?json`);
+    for(let num of numbers){
+        console.log(multiNumbers.data.data[`${num}`]);
+    }
+}
+
+// let numbers = [8, 5, 10];
+// const multiNumbers = axios.get(`${BASE_URL}/${numbers}?json`);
+// multiNumbers
+//     .then(data => {
+//         for(let num of numbers){
+//             console.log(data['data'][`${num}`]);
+//         }
+//     })
 
 let promises = [];
 let fourNums = [1, 2, 3, 4];
